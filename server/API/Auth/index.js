9e10e6c87ce0419a25e612e38b3f9a4120cf67ac
mandar <mandartari9109@gmail.com>
2021-09-19 11:14:31 +0530

    google signin added

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,5 +1,6 @@
 //Library
 import express from 'express';
+import passport from 'passport';
 
 // Models
 import { UserModel } from  "../../database/allModels";
@@ -42,5 +43,36 @@ Router.post('/signin', async (req,res) => {
         return res.status(500).json({error: error.message})
     }
 });
+
+/*
+Route           /auth/google
+Desc            sign with email and password
+Params          none
+Access          Public
+Method          POST
+*/
+Router.get(
+    "/google",
+    passport.authenticate("google",{
+        scope: [
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email",
+        ],
+    })
+);
+
+/*
+Route           /auth/google
+Desc            sign with email and password
+Params          none
+Access          Public
+Method          POST
+*/
+Router.get("/google/callback", 
+passport.authenticate("google", { failureRedirect: "/" }),
+    (req,res) => {
+        return res.json({ token: req.session.passport.user.token});
+    }
+);
 
 export default Router;
