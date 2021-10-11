6db45c23a9d2096c5c982cc6d4b6fbb5c6edb68c
Mandar <mandar@Apples-MacBook.local>
2021-10-11 18:11:00 +0530

    cartBar added

--- client/src/Components/Cart/CartContainer.jsx
+++ client/src/Components/Cart/CartContainer.jsx
@@ -1,103 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-
-//icom
-import { IoIosAdd, IoMdArrowDown, IoMdArrowDropright, IoMdArrowDropup} from "react-icons/io";
+// Icons
+import {
+  IoMdArrowDropdown,
+  IoMdArrowDropright,
+  IoMdArrowDropup,
+} from "react-icons/io";
 import { IoCloseSharp } from "react-icons/io5";
-import FoodItem from '../Restaurant/Order-Online/FoodItem';
+import FoodItem from "./FoodItem";
 
-//components
-function CartSM({toggel}) {
-    return(
-        <>
-        <div className= 'md:hidden flex item-center justify-between'>
-        <div className='flex flex-col item-start'>
-            <small className='flex item-center gap-1'>
-                3 Item <IoMdArrowDropup/>
-            </small>
-            <h4>
-                $300 <sub>(plus tax)</sub>
-            </h4>
-            <button className='flex  item-center gap-1 bg-zomato-400 px-3 py-1 text-white ronded-lg'>
-            Continue <IoMdArrowDropright />
-            </button>
-        </div>
-        </div>
-        </>
-    )
-} 
+// Components
 
-function CartLg({toggel}) {
-    return(
-        <>
-        <div className= 'hidden md:flex item-center justify-between container px-20 mx-auto'>
-        <div className='flex gap-2 text-xl item-start'>
-            <span className='border bg-white border-gray-300 p-1 rounded' onClick={toggel}>
-            <IoMdArrowDropup/>
-            </span>
-            <h4>Your Order (3) </h4>
+function CartSM({ toggle }) {
+  return (
+    <>
+      <div className="md:hidden flex items-center justify-between">
+        <div className="flex flex-col items-start">
+          <small className="flex items-center gap-1">
+            3 Item <IoMdArrowDropup />{" "}
+          </small>
+          <h4>
+            $300 <sub>(plus tax)</sub>
+          </h4>
         </div>
-        <div className='flex item-center gap-2'>
-            <h4 className='text-xl'>Subtotal: $300 </h4>
-            <button className='flex  item-center gap-1 bg-zomato-400 px-3 py-1 text-white ronded-lg'>
+        <button className="flex items-center gap-1 bg-zomato-400 px-3 py-1 text-white rounded-lg">
+          Continue <IoMdArrowDropright />
+        </button>
+      </div>
+    </>
+  );
+}
+
+function CartLg({ toggle }) {
+  return (
+    <>
+      <div className="hidden md:flex items-center justify-between container px-20 mx-auto">
+        <div className="flex gap-2 text-xl items-start">
+          <span
+            className="border bg-white border-gray-300 p-1 rounded"
+            onClick={toggle}
+          >
+            <IoMdArrowDropup />
+          </span>
+          <h4>Your Orders (3)</h4>
+        </div>
+        <div className="flex items-center gap-2">
+          <h4 className="text-xl">Subtotal: $300</h4>
+          <button className="flex items-center gap-1 bg-zomato-400 px-3 py-1 text-white rounded-lg">
             Continue <IoMdArrowDropright />
-            </button>
+          </button>
         </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 function CartContainer() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [cartData, setCartData] = useState([]);
+  const [foods, setFoods] = useState([
+    {
+      image:
+        "https://b.zmtcdn.com/data/dish_photos/87c/153beb91af9f43e157f3d6fd6ea2587c.jpg?output-format=webp",
+      name: "Chilli Paneer Gravy",
+      price: "157.50",
+      quantity: 4,
+    },
+    {
+      image:
+        "https://b.zmtcdn.com/data/dish_photos/87c/153beb91af9f43e157f3d6fd6ea2587c.jpg?output-format=webp",
+      name: "Chilli Paneer Gravy",
+      price: "157.50",
+      quantity: 2,
+    },
+  ]);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const [cartData, setCartData] = useState([]);
-    const [foods, setFoods] = useState([
-        {
-            image:
-              "https://b.zmtcdn.com/data/dish_photos/87c/153beb91af9f43e157f3d6fd6ea2587c.jpg?output-format=webp",
-            name: "Chilli Paneer Gravy",
-            price: "157.50",
-            quantity: 4,
-            descript:
-              "Chicken NoodelsChicken Fried Rice+Chilli ChickenChicken Manchurian+Chilli PotatoHoney Chilli Potato+Chicken Chilli Garlic Momos [2 ... read more",
-          },
-          {
-            image:
-              "https://b.zmtcdn.com/data/dish_photos/87c/153beb91af9f43e157f3d6fd6ea2587c.jpg?output-format=webp",
-            name: "Chilli Paneer Gravy",
-            price: "157.50",
-            quantity: 3,
-            descript:
-              "Chicken NoodelsChicken Fried Rice+Chilli ChickenChicken Manchurian+Chilli PotatoHoney Chilli Potato+Chicken Chilli Garlic Momos [2 ... read more",
-          },
-    ]);
+  const toggleCart = () => setIsOpen((prev) => !prev);
+  const closeCart = () => setIsOpen(false);
 
-    const toggelCart = () => setIsOpen((prev) => !prev);
-    const closeCart = () => setIsOpen(false);
-
-    return (
-        <>
-           {isOpen && (
-               <div className='fixed w-full overflow-y-scroll h-48 bg-white z-10 p-2 bottom-16 px-3'>
-                   <div className='flex item-center justify-between md:px-20'>
-                    <h3 className='text-xl font-semibold'>Your Orders</h3>
-                    <IoCloseSharp onClick={closeCart} />
-                   </div>
-                   <hr className='my-2' />
-                   <div className='flex flex-col gap-2 md:px-20'>
-                    {foods.map((food) => (
-                        <FoodItem {...food} key={foods._id} />
-                    ))};
-                   </div>
-              </div>
-           )} 
-           <div className='fixed w-full bg-white z-10 py-4 px-3 botton-0'>
-               <CartSM toggel={toggelCart} />
-               <CartLg toggel={toggelCart} />        
-         </div>
-        </>
-    )
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed w-full overflow-y-scroll h-48 bg-white z-10 p-2 bottom-16 px-3">
+          <div className="flex items-center justify-between md:px-20">
+            <h3 className="text-xl font-semibold">Your Orders</h3>
+            <IoCloseSharp onClick={closeCart} />
+          </div>
+          <hr className="my-2" />
+          <div className="flex flex-col gap-2 md:px-20">
+            {foods.map((food) => (
+              <FoodItem {...food} key={food._id} />
+            ))}
+          </div>
+        </div>
+      )}
+      <div className="fixed w-full bg-white z-10 py-4 px-3 bottom-0">
+        <CartSM toggle={toggleCart} />
+        <CartLg toggle={toggleCart} />
+      </div>
+    </>
+  );
 }
 
 export default CartContainer;
--- client/src/Components/Cart/FoodItem.jsx
+++ client/src/Components/Cart/FoodItem.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import {BsTrashFill} from 'react-icons/bs'
+import React from "react";
+import { BsTrashFill } from "react-icons/bs";
 
-function FoodItem() {
-    return (
-        <>
-        <div className='flex item-center justify-between'>
+function FoodItem(props) {
+  return (
+    <>
+      <div className="flex items-center justify-between">
         <h5>{props.name}</h5>
-            <div className='flex item-center gap-2'>
-                
-                  <div className='flex flex-col items-end'>
-                    <small>$ {props.price}</small>
-                    <div className=' px-1 bg-zomato-400 text-white rounded flex item-center gap-1'>
-                        <button className='p-1 bg-zomato-400 text-white rounded'>
-                            -
-                        </button>
-                        <small>
-                            {props.quantity}
-                        </small>
-                        <button className='p-1 bg-zomato-400 text-white rounded'>
-                            +
-                        </button>
-                    </div>
-                  </div>
-                  <BsTrashFill className='text-zomato-400 text-lg md:text-xl ' />
-                </div>
-         </div>
-         <hr className='my-2' />
-        </>
-    )
+        <div className="flex items-center gap-2">
+          <div className="flex flex-col items-end">
+            <small>$ {props.price}</small>
+            <div className="px-1 bg-zomato-400 text-white rounded flex items-center gap-1">
+              <button className="p-1 bg-zomato-400 text-white rounded">
+                -
+              </button>
+              <small>{props.quantity}</small>
+              <button className="p-1 bg-zomato-400 text-white rounded">
+                +
+              </button>
+            </div>
+          </div>
+          <BsTrashFill className="text-zomato-400 text-lg md:text-xl" />
+        </div>
+      </div>
+      <hr className="my-2" />
+    </>
+  );
 }
 
 export default FoodItem;
